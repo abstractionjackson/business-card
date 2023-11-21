67d63d2f9c5ef6391bbf6df940d7de9a75a17df1
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-11-20 21:54:33 -0800

    locate styles in js, and update layout

--- businessCard.js
+++ businessCard.js
@@ -11,11 +11,53 @@ class BusinessCard extends HTMLElement {
     const phone = this.getAttribute('phone')
 
     this.shadowRoot.innerHTML = `
+            <style>
+                .card {
+                    font-family: 'Helvetica', sans-serif;
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    grid-template-rows: 1fr 2fr 1fr;
+                    justify-items: center;
+                    align-items: center;
+                    max-width: 350px;
+                    margin: 20px auto;
+                    padding: 20px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    border-radius: 8px;
+                    background-color: #fff;
+                    position: relative;
+                    height: 200px;
+                }
+
+                .phone {
+                    grid-column: 1;
+                    grid-row: 1;
+                    justify-self: start;
+                }
+
+                .company {
+                    grid-column: 2;
+                    grid-row: 1;
+                    justify-self: end;
+                }
+
+                .name {
+                    grid-column: 1 / -1;
+                    font-size: 24px;
+                    color: black;
+                }
+
+                .title {
+                    grid-column: 1 / -1;
+                    font-size: 18px;
+                    color: #444;
+                }
+            </style>
             <div class="card">
-                <h1>${name}</h1>
-                <p>${title}</p>
-                <p>${company}</p>
-                <p>${phone}</p>
+                <p class="phone">${phone}</p>
+                <p class="company">${company}</p>
+                <h1 class="name">${name}</h1>
+                <p class="title">${title}</p>
             </div>
         `
   }
