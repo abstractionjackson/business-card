3ad216f056199bebe3948dd8f63bf03d145cddaf
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-11-25 09:33:08 -0800

    add getStaticProps and uuid4

--- businessCard/utils.js
+++ businessCard/utils.js
@@ -1,6 +1,6 @@
-export function formatPhoneNumber(phoneNumber) {
+export const formatPhoneNumber = (phoneNumber) => {
   // Remove any spaces from the phone number
-  phoneNumber = phoneNumber.replace(/\s/g, '')
+  phoneNumber = phoneNumber.toString().replace(/\s/g, '')
 
   // Check if the phone number is 10 digits long
   if (phoneNumber.length !== 10) {
@@ -16,4 +16,18 @@ export function formatPhoneNumber(phoneNumber) {
     '-' +
     phoneNumber.substring(6)
   )
+}
+
+const excludedProps = new Set(['length', 'name', 'prototype'])
+export const getStaticProperties = (Class) =>
+  Object.getOwnPropertyNames(Class).filter((prop) => {
+    return typeof Class[prop] !== 'function' && !excludedProps.has(prop)
+  })
+
+export const uuid4 = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }
