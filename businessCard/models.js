d79109995f84ea63212f050f117249be38eb888f
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2023-11-25 09:33:57 -0800

    add getters and setters, refactor constructor

--- businessCard/models.js
+++ businessCard/models.js
@@ -1,66 +1,111 @@
 // businessCard.js
 import { cardStyles, textStyles, containerStyles } from './styles.js'
-import { formatPhoneNumber } from './utils.js'
+import {
+  formatPhoneNumber,
+  getStaticProperties,
+  uuid4 as uuid,
+} from './utils.js'
 
 export class BusinessCard extends HTMLElement {
+  static name_first
+  static name_last
+  static phone
+  static company_name
+  static job_title
+  static email
+  // static linkedin
+
   constructor() {
     super()
+    // initialize the shadow
     this.attachShadow({ mode: 'open' })
-    this.createCardContent()
+    // instantiate the card from custom element props
+    for (const prop of getStaticProperties(BusinessCard)) {
+      this[`_${prop}`] = this.getAttribute(prop)
+    }
+    // initialize the HTML
+    this.init()
+    // place the HTML on the shadow
+    this.shadowRoot.appendChild(this.content)
   }
 
+  init() {
+    // Get all HTML Nodes
+
+    // The Content Container
+    const contentContainer = this.getContentContainer()
+
+    // Order (?)
+    for (const prop of getStaticProperties(BusinessCard)) {
+      contentContainer.appendChild(this[prop])
+    }
+
+    this.content = contentContainer // assign new static prop (?)
+
+    return contentContainer
+  }
+
+  getContentContainer() {
+    const el = document.createElement('div')
+    el.id = `${uuid()}`
+    return el
+  }
+  get phone() {
+    const link = document.createElement('a')
+    link.href = `tel:1${this._phone}` // USA
+    link.innerText = formatPhoneNumber(this._phone)
+    return link
+  }
+  set phone(value) {
+    // TODO - add validation
+    this._phone = value
+  }
+  get email() {
+    const link = document.createElement('a')
+    link.href = `mailto:${this._email}`
+    link.innerText = this._email
+    return link
+  }
+  set email(value) {
+    this._email = value
+  }
+  get name_last() {
+    const span = document.createElement('span')
+    span.innerText = this._name_last
+    return span
+  }
+  set name_last(value) {
+    this._name_last = value
+  }
+  get name_first() {
+    const span = document.createElement('span')
+    span.innerText = this._name_first
+    return span
+  }
+  set name_first(value) {
+    this._name_first = value
+  }
+  get job_title() {
+    const span = document.createElement('span')
+    span.innerText = this._job_title
+    return span
+  }
+  set job_title(value) {
+    this._job_title = value
+  }
+  get company_name() {
+    const span = document.createElement('span')
+    span.innerText = this._company_name
+    return span
+  }
+  set company_name(value) {
+    this._company_name = value
+  }
   applyStyles(element, styles) {
     for (const property in styles) {
       element.style[property] = styles[property]
     }
   }
-
-  createCardContent() {
-    const card = document.createElement('div')
-    this.applyStyles(card, cardStyles)
-
-    const phoneNumber = this.getAttribute('phone')
-    const phoneNumberF = formatPhoneNumber(phoneNumber)
-    const phone = this.createTextElement('a', phoneNumberF, textStyles.phone)
-    phone.setAttribute('href', `tel:+1${phoneNumber}`) //USA
-
-    const company = this.createTextElement(
-      'p',
-      this.getAttribute('company'),
-      textStyles.company,
-    )
-    const nameAndTitleContainer = document.createElement('div')
-    this.applyStyles(nameAndTitleContainer, containerStyles.nameAndTitle)
-
-    const nameLast = this.createTextElement(
-      'span',
-      this.getAttribute('name').split(' ').pop(),
-      textStyles.name.last,
-    )
-    const name = this.createTextElement(
-      'span',
-      this.getAttribute('name').split(' ').slice(0, -1).join(' '),
-      textStyles.name.ante,
-    )
-    const nameHeading = document.createElement('h1')
-    this.applyStyles(nameHeading, textStyles.name.heading)
-    const title = this.createTextElement(
-      'p',
-      this.getAttribute('title'),
-      textStyles.title,
-    )
-
-    nameHeading.appendChild(name)
-    nameHeading.appendChild(nameLast)
-    nameAndTitleContainer.appendChild(nameHeading)
-    nameAndTitleContainer.appendChild(title)
-    card.appendChild(phone)
-    card.appendChild(company)
-    card.appendChild(nameAndTitleContainer)
-
-    this.shadowRoot.appendChild(card)
-  }
-
   createTextElement(tag, text, styles) {
     const element = document.createElement(tag)
     element.textContent = text
