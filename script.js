45a24ee740c80316e967461a8c67db9f57f39bb3
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2024-04-10 18:05:43 +0000

    update page, component styles

--- script.js
+++ script.js
@@ -8,5 +8,9 @@ class extends HTMLElement {
         const templatecontent = template.content
 
         this.attachShadow({mode: 'open'}).appendChild(templatecontent.cloneNode(true))
+
+        // set the first and last name from #name-first, #name-last
+        this.shadowRoot.querySelector('#name-first').textContent = this.getAttribute('first')
+        this.shadowRoot.querySelector('#name-last').textContent = this.getAttribute('last')
     }
 })
