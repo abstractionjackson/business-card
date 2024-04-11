e739e14bbb232cd969e203a0602e242c89a57fd2
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2024-04-11 13:12:22 -0700

    make content editable

--- script.js
+++ script.js
@@ -8,9 +8,32 @@ class extends HTMLElement {
         const templatecontent = template.content
 
         this.attachShadow({mode: 'open'}).appendChild(templatecontent.cloneNode(true))
+        
+        // ensure that the content is editable (every contenteditable), and has the correct focus behavior
+        const editable = this.shadowRoot.querySelectorAll('[contenteditable]')
+        for (let i = 0; i < editable.length; i++) {
+            addFocusBehavior(editable[i]) ;
+        }
 
-        // set the first and last name from #name-first, #name-last
-        this.shadowRoot.querySelector('#name-first').textContent = this.getAttribute('first')
-        this.shadowRoot.querySelector('#name-last').textContent = this.getAttribute('last')
+        // ensure that the content is editable (every slot), and has the correct focus behavior
+        const slots = this.shadowRoot.querySelectorAll('slot')
+        slots.forEach(slot => {
+            slot.assignedNodes().forEach(node => {
+                node.contentEditable = true
+                addFocusBehavior(node) // user-added HTML needs event-listener
+            })
+        });
+
     }
 })
+
+function addFocusBehavior(element) {
+    element.addEventListener('focus', function(e) {
+        const range = document.createRange(); // Create a range
+        range.selectNodeContents(e.target); // Select the entire contents of the element
+    
+        const sel = window.getSelection(); // Get the current selection
+        sel.removeAllRanges(); // Remove any current selections
+        sel.addRange(range); // Add the new range
+    });
+};
